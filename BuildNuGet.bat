71edd8e0bc24ff2f54724cb96f2825274012e512
Jorn <jw@elfisk.dk>
2020-06-23 08:03:45 +0200

    NuGet release.

--- BuildNuGet.bat
+++ BuildNuGet.bat
@@ -1,1 +1,5 @@
+REM 1) Update versions in Macaroons.Net AssemblyInfo.cs
+REM 2) Update release note in Macaroons.Net.nuspec
+REM 3) Run this script
+
 \bin\nuget.exe pack -OutputFileNamesWithoutVersion Macaroons.net
