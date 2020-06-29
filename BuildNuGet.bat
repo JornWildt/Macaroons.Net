723ebe3870325335cedadecb1a7fa5577080a8e3
Jorn <jw@elfisk.dk>
2020-06-29 08:16:03 +0200

    New release 1.0.3. Trying to build strong named assembly, but it was not possible since the libsodium-net is not strong named.

--- BuildNuGet.bat
+++ BuildNuGet.bat
@@ -1,5 +1,7 @@
-REM 1) Update versions in Macaroons.Net AssemblyInfo.cs
-REM 2) Update release note in Macaroons.Net.nuspec
-REM 3) Run this script
+REM 1) Update versions in Macaroons.Net AssemblyInfo.cs.
+REM 2) Select "Release" mode for compilation.
+REM 3) Update release note in Macaroons.Net.nuspec.
+REM 4) Compile
+REM 5) Run this script
 
 \bin\nuget.exe pack -OutputFileNamesWithoutVersion Macaroons.net
