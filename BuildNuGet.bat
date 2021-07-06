be61672fe62c2362e9645c74db0183a3f56df5d3
Jorn <jw@elfisk.dk>
2021-07-06 22:10:15 +0200

    Release NuGet version 2.0.0.

--- BuildNuGet.bat
+++
@@ -1,7 +0,0 @@
-REM 1) Update versions in Macaroons.Net AssemblyInfo.cs.
-REM 2) Select "Release" mode for compilation.
-REM 3) Update release note in Macaroons.Net.nuspec.
-REM 4) Compile
-REM 5) Run this script
-
-\bin\nuget.exe pack -OutputFileNamesWithoutVersion Macaroons.net
